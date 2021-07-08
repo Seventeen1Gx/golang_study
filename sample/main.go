ccd02e4b0d7d1542e095fec911c3a34cd307c6c0
gaoxiang.fighting <gaoxiang.fighting@bytedance.com>
2021-07-08 21:53:15 +0800

    finish code

--- sample/main.go
+++ sample/main.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	_ "github.com/Seventeen1Gx/golang_study/sample/matchers"
+	"github.com/Seventeen1Gx/golang_study/sample/search"
+	"log"
+	"os"
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+}
+
+func main() {
+	search.Run("president")
+}
